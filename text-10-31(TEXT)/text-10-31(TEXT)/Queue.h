9f11a50d0422706805178ab88dda356fd1fb8dc3
李女女 <liwenfeng20210403@163.com>
2021-11-02 19:39:33 +0800

    队列代码课件

---
+++ text-10-31(TEXT)/text-10-31(TEXT)/Queue.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+typedef int QDataType;
+
+typedef struct QueueNode
+{
+	struct QueueNode* next;
+	QDataType data;
+}QueueNode;
+
+typedef struct Queue
+{
+	QueueNode* head;
+	QueueNode* tail;
+	// size_t _size;
+}Queue;
+
+//void QueueInit(QueueNode** pphead, QueueNode** pptail);
+void QueueInit(Queue* pq);
+void QueueDestroy(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+int QueueSize(Queue* pq);
+bool QueueEmpty(Queue* pq);
