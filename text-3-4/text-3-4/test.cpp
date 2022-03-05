43ef049591bf32856275e3ed6e6fd305838e3cd6
李女女 <liwenfeng20210403@163.com>
2022-03-05 11:33:27 +0800

    C++入门

---
+++ text-3-4/text-3-4/test.cpp
@@ -0,0 +1,18 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	double b;
+	char c;
+
+	cin >> a;
+	cin >> b >> c;
+	cout << a << endl;
+	cout << b << "  " << c << endl;
+	printf("%d",a);
+	return 0;
+}
+
